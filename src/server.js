98d6d2eebcdcdc6e57a593de03ecbe1e123ad04f
Ibrahim Aji <112231127+ajiibrahim01@users.noreply.github.com>
2024-06-18 17:26:31 +0700

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi'); // import module hapi for build a web server
+const routes = require('./routes'); // import module from routes.js
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
